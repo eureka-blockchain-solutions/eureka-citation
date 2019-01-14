98afbf409342c2f4e8620a6c31793be2deb24797
lucaspelloni2 <lucas_pelloni@hotmail.com>
2019-01-14 13:52:22 +0100

    remove unused stuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React, { Component } from "react";
-import Alert from "./views/design-components/Alerts";
-import Modal from "./views/design-components/Modal";
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Alert status={"success"} iconSize={25}>
-          This is a success alert
-        </Alert>
-        <Modal
-          type={"notification"}
-          toggle={isErrorMessage => {
-            this.setState({ errorMessage: null });
-          }}
-          show={true}
-          title={"You got the following error"}
-        >
-          apkosfjoasfjoasjo
-        </Modal>
 
 
       </div>
