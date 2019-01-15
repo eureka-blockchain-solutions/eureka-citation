594fbfe6cf34a13831054dadff5d539b31e3ee5b
lucaspelloni2 <lucas_pelloni@hotmail.com>
2019-01-15 15:15:01 +0100

    markdown for code and template

--- src/views/EncodingResult.js
+++ src/views/EncodingResult.js
@@ -3,12 +3,18 @@ import styled from "styled-components";
 import EurekaAddress from "./EurekaAddress";
 import LatexCitation from "./LatexCitation";
 import EKAQRCode from "./QRCode";
+import Markdown from "react-markdown";
 import Icon from "./icons/Icon";
+import { __GRAY_100 } from "../helpers/colors";
+import { Separator } from "./Separator";
 
 const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
   flex-direction: column;
+  background: ${__GRAY_100};
+  padding: 20px 8px;
+  margin-top: 10px;
 `;
 
 const SubContainer = styled.div`
@@ -19,17 +25,29 @@ const Header = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
 `;
 const Title = styled.h3`
   text-align: center;
+  font-size: 14px;
+  font-family: monospace;
+  margin-bottom: -15px;
 `;
 const EncodingResult = ({ ethAddress, encodedAddress }) => {
+  const markdown = `
+        ${ethAddress}
+    `;
   return (
     <Container>
       <Header>
-        <Title>{ethAddress}</Title>
-        <Icon icon={"ethereum"} width={15} height={15} />
+        <Title>Evaluating the following address:</Title>
+        <Markdown
+          source={markdown}
+          escapeHtml={false}
+          style={{ marginTop: 0 }}
+        />
       </Header>
+      <Separator bottom={30} top={15} width={"30%"} height={"5px"} />
       <SubContainer>
         <EurekaAddress encodedAddress={encodedAddress} />
         <EKAQRCode encodedAddress={encodedAddress} />
--- src/views/Separator.js
+++ src/views/Separator.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
-import {__FIFTH, __THIRD} from "../helpers/colors";
+import { __FIFTH, __THIRD } from "../helpers/colors";
 
 export const Separator = styled.div`
-  width: 100%;
-  height: 2.5px;
+  width: ${props => (props.width ? props.width : "100%")};
+  height: ${props => (props.height ? props.height : "2.5px")};
   margin-top: ${props => props.top}px;
   margin-bottom: ${props => props.bottom}px;
   background-image: linear-gradient(120deg, ${__THIRD} 0%, ${__FIFTH} 100%);
   border-radius: 15px;
+  align-self: center;
 `;
