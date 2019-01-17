301b68c5ec024d5dd4186b1b22e619d5cdd02857
lucaspelloni2 <lucas_pelloni@hotmail.com>
2019-01-17 10:45:30 +0100

    Functional Header

---
+++ src/views/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  min-height: 90px;
+  background: black;
+`;
+
+const Header = () => {
+  return <Container>Header Functional Component</Container>;
+};
+
+export default Header;
